d083c1b976c4280571227a67dd0decde0c34bd2d
AndrewZerfas <89168481+AndrewZerfas@users.noreply.github.com>
2022-11-07 11:43:42 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask_app import app
 from flask_app.controllers import andycontroller
 
-from waitress import serve
+#from waitress import serve
 
-#if __name__=="__main__":
-#    app.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True)
 
-serve(app, host='0.0.0.0', port=8080, threads=1)
+#serve(app, host='0.0.0.0', port=8080, threads=1)
 
